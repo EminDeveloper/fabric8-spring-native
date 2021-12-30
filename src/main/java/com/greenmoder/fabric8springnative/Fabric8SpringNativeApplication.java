6788d604ef19a74cef67a1f681a2d641d7344a57
Emin Developer <emin.f.mammadov@gmail.com>
2021-12-29 18:24:31 -0800

    Fabric8

---
+++ src/main/java/com/greenmoder/fabric8springnative/Fabric8SpringNativeApplication.java
@@ -0,0 +1,13 @@
+package com.greenmoder.fabric8springnative;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fabric8SpringNativeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Fabric8SpringNativeApplication.class, args);
+    }
+
+}
